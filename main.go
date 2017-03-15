516b1ebbbd82e6d2d115d744773dccd1a3631741
mkolas <kolasinskim@gmail.com>
2017-03-14 23:38:27 -0400

    First pass and working

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"encoding/csv"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -18,20 +19,96 @@ func init() {
 }
 
 var token string
-var buffer = make([][]byte, 0)
+
+var (
+	// discordgo session
+	discord *discordgo.Session
+
+	// Map of Guild id's to *Play channels, used for queuing and rate-limiting guilds
+	queues map[string]chan *Play = make(map[string]chan *Play)
+
+	sounds = []*Sound{}
+
+	soundMap = map[string]*Sound{}
+
+	// Sound encoding settings
+	BITRATE        = 128
+	MAX_QUEUE_SIZE = 6
+
+	// Owner
+	OWNER string
+)
+
+// Play represents an individual use of the !airhorn command
+type Play struct {
+	GuildID   string
+	ChannelID string
+	UserID    string
+	Sound     *Sound
+}
+
+
+// Sound type cribbed from airhornbot.
+type Sound struct {
+	Name string `csv:"filename"`
+
+	// major difference here is that we want to be able to call each sound explicitly
+	Command string `csv:"command"`
+
+	// Really not sure how important this is. let's defa
+	PartDelay int `csv:"-"`
+
+	// Buffer to store encoded PCM packets
+	buffer [][]byte `csv:"-"`
+}
 
 func main() {
+
+	// Instead, let's pull a token from a configuration file.
 	if token == "" {
 		fmt.Println("No token provided. Please run: buster -t <bot token>")
 		return
 	}
 
-	// Load the sound file.
-	err := loadSound()
+	// cribbed from goscv example
+	soundsFile, err := os.OpenFile("config/sounds.csv", os.O_RDWR|os.O_CREATE, os.ModePerm) // should figure out what these os objects are
 	if err != nil {
-		fmt.Println("Error loading sound: ", err)
-		fmt.Println("Please copy $GOPATH/src/github.com/bwmarrin/examples/airhorn/airhorn.dca to this directory.")
-		return
+		panic(err)
+	}
+	defer soundsFile.Close()
+
+	reader := csv.NewReader(soundsFile)
+	//Configure reader options Ref http://golang.org/src/pkg/encoding/csv/reader.go?s=#L81
+	reader.Comma = ','          //field delimiter
+	reader.Comment = '#'        //Comment character
+	reader.FieldsPerRecord = 2 //Number of records per record. Set to Negative value for variable
+	reader.TrimLeadingSpace = true
+
+	for {
+		// read just one record, but we could ReadAll() as well
+		record, err := reader.Read()
+		// end-of-file is fitted into err
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			reader.Read()
+			continue
+		}
+		// record is array of strings Ref http://golang.org/src/pkg/encoding/csv/reader.go?s=#L134
+			// Create the play
+		sound := &Sound{
+			Name:   record[0],
+			Command: record[1],
+		}
+		sounds = append(sounds, sound)
+	}
+
+	for _, sound := range sounds {
+		// for each sound, load the .dca into memory and store it in the Sound struct
+		sound.Load()
+		soundMap[sound.Command] = sound
+		fmt.Println("Loaded filename", sound.Name, "loaded command", sound.Command)
 	}
 
 	// Create a new Discord session using the provided bot token.
@@ -56,7 +133,7 @@ func main() {
 		fmt.Println("Error opening Discord session: ", err)
 	}
 
-	fmt.Println("Buster is now running.  Press CTRL-C to exit.")
+	fmt.Println("Bobby is now running.  Press CTRL-C to exit.")
 	// Simple way to keep program running until CTRL-C is pressed.
 	<-make(chan struct{})
 	return
@@ -70,8 +147,9 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if strings.HasPrefix(m.Content, "!bobby") {
-		// Find the channel that the message came from.
+	if strings.HasPrefix(m.Content, "!") { // we can make the prefix configurable but for right now always look for !
+		command := m.Content[1:] //substring starting at index 1
+
 		c, err := s.State.Channel(m.ChannelID)
 		if err != nil {
 			// Could not find channel.
@@ -85,17 +163,18 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 
-		// Look for the message sender in that guilds current voice states.
-		for _, vs := range g.VoiceStates {
-			if vs.UserID == m.Author.ID {
-				err = playSound(s, g.ID, vs.ChannelID)
-				if err != nil {
-					fmt.Println("Error playing sound:", err)
-				}
-
+		// get audio channel to play in
+		ac := getCurrentVoiceChannel(m.Author, g, s)
+			if ac == nil {
+				fmt.Println("Failed to find channel to play sound in")
 				return
-			}
 		}
+
+		i, ok := soundMap[command] // look for command in our soundMap
+		if ok { // we found it, so lets queue the sound
+			go enqueuePlay(m.Author, ac, g, i, s)
+		}
+		return
 	}
 }
 
@@ -108,83 +187,148 @@ func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 
 	for _, channel := range event.Guild.Channels {
 		if channel.ID == event.Guild.ID {
-			_, _ = s.ChannelMessageSend(channel.ID, "It's Bobby Moynihan is ready! Type !bobby while in a voice channel to play a sound.")
+			_, _ = s.ChannelMessageSend(channel.ID, "Bobby Moynihan is ready! Type !bobby while in a voice channel to play a sound.")
 			return
 		}
 	}
 }
 
-// loadSound attempts to load an encoded sound file from disk.
-func loadSound() error {
-	file, err := os.Open("bobby.dca")
+// Load attempts to load an encoded sound file from disk
+// DCA files are pre-computed sound files that are easy to send to Discord.
+// If you would like to create your own DCA files, please use:
+// https://github.com/nstafie/dca-rs
+// eg: dca-rs --raw -i <input wav file> > <output file>
+func (s *Sound) Load() error {
+	path := "sounds/"+s.Name
+
+	file, err := os.Open(path)
 
 	if err != nil {
-		fmt.Println("Error opening dca file :", err)
+		fmt.Println("error opening dca file :", err)
 		return err
 	}
 
 	var opuslen int16
 
 	for {
-		// Read opus frame length from dca file.
+		// read opus frame length from dca file
 		err = binary.Read(file, binary.LittleEndian, &opuslen)
 
-		// If this is the end of the file, just return.
+		// If this is the end of the file, just return
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			err := file.Close()
-			if err != nil {
-				return err
-			}
 			return nil
 		}
 
 		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
+			fmt.Println("error reading from dca file :", err)
 			return err
 		}
 
-		// Read encoded pcm from dca file.
+		// read encoded pcm from dca file
 		InBuf := make([]byte, opuslen)
 		err = binary.Read(file, binary.LittleEndian, &InBuf)
 
 		// Should not be any end of file errors
 		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
+			fmt.Println("error reading from dca file :", err)
 			return err
 		}
 
-		// Append encoded pcm data to the buffer.
-		buffer = append(buffer, InBuf)
+		// append encoded pcm data to the buffer
+		s.buffer = append(s.buffer, InBuf)
 	}
 }
 
-// playSound plays the current buffer to the provided channel.
-func playSound(s *discordgo.Session, guildID, channelID string) (err error) {
-	// Join the provided voice channel.
-	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
-	if err != nil {
-		return err
+// Prepares and enqueues a play into the ratelimit/buffer guild queue
+func enqueuePlay(user *discordgo.User, channel *discordgo.Channel, guild *discordgo.Guild, sound *Sound, session *discordgo.Session) {
+	play := createPlay(user, channel, guild, sound)
+	if play == nil {
+		return
+	}
+
+	// Check if we already have a connection to this guild
+	//   yes, this isn't threadsafe, but its "OK" 99% of the time
+	_, exists := queues[guild.ID]
+
+	if exists {
+		if len(queues[guild.ID]) < MAX_QUEUE_SIZE {
+			queues[guild.ID] <- play
+		}
+	} else {
+		queues[guild.ID] = make(chan *Play, MAX_QUEUE_SIZE)
+		playSound(play, nil, session)
+	}
+}
+
+// Prepares a play
+func createPlay(user *discordgo.User, channel *discordgo.Channel, guild *discordgo.Guild, sound *Sound) *Play {
+
+	// Create the play
+	play := &Play{
+		GuildID:   guild.ID,
+		ChannelID: channel.ID,
+		UserID:    user.ID,
+		Sound:     sound,
+	}
+
+	return play
+}
+
+// Play a sound
+func playSound(play *Play, vc *discordgo.VoiceConnection, session *discordgo.Session) (err error) {
+	fmt.Println("playing sound"+play.Sound.Name)
+
+	if vc == nil {
+		vc, err = session.ChannelVoiceJoin(play.GuildID, play.ChannelID, false, false)
+		// vc.Receive = false
+		if err != nil {
+			fmt.Println("Failed to play sound")
+		}
+	}
+
+	// If we need to change channels, do that now
+	if vc.ChannelID != play.ChannelID {
+		vc.ChangeChannel(play.ChannelID, false, false)
+		time.Sleep(time.Millisecond * 125)
 	}
 
 	// Sleep for a specified amount of time before playing the sound
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(time.Millisecond * 32)
 
-	// Start speaking.
-	_ = vc.Speaking(true)
+	// Play the sound
+	play.Sound.Play(vc)
 
-	// Send the buffer data.
-	for _, buff := range buffer {
-		vc.OpusSend <- buff
+	// If there is another song in the queue, recurse and play that
+	if len(queues[play.GuildID]) > 0 {
+		play := <-queues[play.GuildID]
+		playSound(play, vc, session)
+		return nil
 	}
 
-	// Stop speaking
-	_ = vc.Speaking(false)
+	// If the queue is empty, delete it
+	time.Sleep(time.Millisecond * time.Duration(play.Sound.PartDelay))
+	delete(queues, play.GuildID)
+	vc.Disconnect()
+	return nil
+}
 
-	// Sleep for a specificed amount of time before ending.
-	time.Sleep(250 * time.Millisecond)
+// Plays this sound over the specified VoiceConnection
+func (s *Sound) Play(vc *discordgo.VoiceConnection) {
+	vc.Speaking(true)
+	defer vc.Speaking(false)
 
-	// Disconnect from the provided voice channel.
-	_ = vc.Disconnect()
+	for _, buff := range s.buffer {
+		vc.OpusSend <- buff
+	}
+}
 
+// Attempts to find the current users voice channel inside a given guild
+func getCurrentVoiceChannel(user *discordgo.User, guild *discordgo.Guild, session *discordgo.Session) *discordgo.Channel {
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == user.ID {
+			channel, _ := session.State.Channel(vs.ChannelID)
+			return channel
+		}
+	}
 	return nil
 }
